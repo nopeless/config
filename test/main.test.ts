dae501680dfc7276fa167b4031e49fb19a444b81
nopeless <38830903+nopeless@users.noreply.github.com>
2023-05-16 21:55:29 +0900

    opinionated updates

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,8 @@
-import { test } from "@src";
+import { test } from "nopeless-config";
 
 describe(`main`, function () {
   it(`should work`, function () {
+    expect(process.env).to.not.be.null;
     expect(test.param).to.equal(`test`);
   });
 });
