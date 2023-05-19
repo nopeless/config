e27a545761aafd42847058df1ef86e467761d1e4
nopeless <38830903+nopeless@users.noreply.github.com>
2023-05-19 17:04:59 +0900

    bring back mocharc

---
+++ .mocharc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  "loader": "tsx",
+  "files": ["test/**/*.spec.ts", "test/**/*.test.ts"],
+  "recursive": true,
+  "extension": [
+      "ts"
+  ],
+  "require": "test/prelude.js"
+}
